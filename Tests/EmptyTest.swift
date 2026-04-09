b71c0be2c19104e58718dde6e345fd91c0e083f2
chingor <chingor@google.com>
2026-04-09 13:46:16 -0700

    feat: add GoogleCloudWkt.Empty implementation (#37)
    Fixes #30

---
+++ Tests/EmptyTest.swift
@@ -0,0 +1,43 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import GoogleCloudWkt
+import Testing
+
+struct WrappedEmptyEncode: Encodable {
+  let value: GoogleCloudWkt.Empty
+}
+
+@Test("Encoding JSON")
+func testEncodingJSON() throws {
+  let wrapped = WrappedEmptyEncode(value: GoogleCloudWkt.Empty())
+  let encoder = JSONEncoder()
+  let data = try encoder.encode(wrapped)
+  let jsonString = String(data: data, encoding: .utf8)
+  #expect(jsonString == "{\"value\":{}}")
+}
+
+struct WrappedEmptyDecode: Decodable {
+  let value: GoogleCloudWkt.Empty
+}
+
+@Test("Decoding JSON")
+func testDecodingJSON() throws {
+  let jsonString = "{\"value\":{}}"
+  let data = jsonString.data(using: .utf8)!
+  let decoder = JSONDecoder()
+  let wrapped = try decoder.decode(WrappedEmptyDecode.self, from: data)
+  #expect(wrapped.value == Empty())
+}
