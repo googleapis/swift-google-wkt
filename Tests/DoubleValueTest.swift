6147e729d11ff4f51d8f1137172580cb473818d7
Jeff Ching <chingor@google.com>
2026-04-30 16:25:40 -0700

    feat: implement DoubleValue (#82)
    This option implements DoubleValue as an alias of `Optional<Double>`
    with an `_AnyPackable` extension
    
    Towards #59

---
+++ Tests/DoubleValueTest.swift
@@ -0,0 +1,106 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import GoogleCloudWkt
+import Testing
+
+@Suite struct DoubleValueTests {
+  struct WrappedDoubleValueEncode: Encodable {
+    let value: GoogleCloudWkt.DoubleValue
+  }
+
+  @Test(
+    "DoubleValue JSON Encoding",
+    arguments: [
+      (123.45, "{\"value\":123.45}"),
+      (0, "{\"value\":0}"),
+    ])
+  func encodeJSON(_ args: (Double, String)) throws {
+    let wrapped = WrappedDoubleValueEncode(value: args.0)
+    let encoder = JSONEncoder()
+    let data = try encoder.encode(wrapped)
+    let got = String(data: data, encoding: .utf8)!
+    #expect(got == args.1)
+  }
+
+  @Test("DoubleValue JSON Encoding unset")
+  func encodeJSONUnset() throws {
+    let wrapped = WrappedDoubleValueEncode(value: nil)
+    let encoder = JSONEncoder()
+    let data = try encoder.encode(wrapped)
+    let got = String(data: data, encoding: .utf8)!
+    #expect(got == "{}")
+  }
+
+  struct WrappedDoubleValueDecode: Decodable {
+    let value: GoogleCloudWkt.DoubleValue
+  }
+
+  @Test(
+    "DoubleValue JSON Decoding",
+    arguments: [
+      ("{\"value\":123.45}", 123.45),
+      ("{\"value\":0}", 0),
+    ])
+  func decodeJSON(_ args: (String, Double)) throws {
+    let data = args.0.data(using: .utf8)!
+    let decoder = JSONDecoder()
+    let wrapped = try decoder.decode(WrappedDoubleValueDecode.self, from: data)
+    #expect(wrapped.value == args.1)
+  }
+
+  @Test(
+    "DoubleValue JSON Decoding unset")
+  func decodeJSONUnset() throws {
+    let data = "{}".data(using: .utf8)!
+    let decoder = JSONDecoder()
+    let wrapped = try decoder.decode(WrappedDoubleValueDecode.self, from: data)
+    #expect(wrapped.value == nil)
+  }
+
+  struct WrappedAny: Codable {
+    let content: GoogleCloudWkt.`Any`
+  }
+
+  @Test("Unpack DoubleValue from Any")
+  func doubleValueAnyUnpack() throws {
+    let jsonString =
+      #"{"content":{"@type":"type.googleapis.com/google.protobuf.DoubleValue","value":123.45}}"#
+    let data = jsonString.data(using: .utf8)!
+    let decoder = JSONDecoder()
+    let wrapped = try decoder.decode(DoubleValueTests.WrappedAny.self, from: data)
+    let any = wrapped.content
+    #expect(any.typeUrl == "type.googleapis.com/google.protobuf.DoubleValue")
+
+    let got = try DoubleValue(fromAny: any)
+    let want = 123.45
+    #expect(got == want)
+  }
+
+  @Test("Pack DoubleValue into Any")
+  func doubleValueAnyPack() throws {
+    let input = DoubleValue(123.45)
+    let any = try `Any`(fromMessage: input)
+    let wrapped = DoubleValueTests.WrappedAny(content: any)
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = [.sortedKeys, .withoutEscapingSlashes]
+    let data = try encoder.encode(wrapped)
+    let got = String(data: data, encoding: .utf8)!
+
+    let want =
+      #"{"content":{"@type":"type.googleapis.com/google.protobuf.DoubleValue","value":123.45}}"#
+    #expect(got == want)
+  }
+}
