ef2536431bbc2c19f0a88b6ace36d2d100840e3c
Jeff Ching <chingor@google.com>
2026-09-22 21:52:36 +0000

    fix(wkt)!: ProtoJSONError does not need to be public (#1055)
    Fixes #1034

--- Sources/GoogleWKT/ProtoJSONDecoder.swift
+++ Sources/GoogleWKT/ProtoJSONDecoder.swift
@@ -271,6 +271,6 @@ extension InternalUnkeyedDecodingContainer: UnkeyedDecodingContainer {
 /// - Note: As Google Cloud APIs and client libraries evolve, new error cases may be added to this
 ///   enumeration in minor or patch releases. Always handle unexpected cases using an `@unknown default:`
 ///   clause in `switch` statements.
-public enum ProtoJSONError: Error {
+enum ProtoJSONError: Error {
   case unsupportedType(String)
 }
